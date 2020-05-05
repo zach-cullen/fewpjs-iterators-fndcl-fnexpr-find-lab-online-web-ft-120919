a67feba56262bbdab7546dc6aec2177e39646357
Zach Cullen <zacullen@gmail.com>
2020-05-05 09:20:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const testVar = {}
+// const record = [
+//   {year: "2018", result: "L"},
+//   {year: "2017", result: "W"},
+//   {year: "2016", result: "N/A"}
+// ]
 
-function testFunc() {
-  return "hi"
+function superbowlWin(record) {
+  const win = record.find(game => game.result === "W")
+  return win ? win.year : win
 }
